5743e71d710bc76df81217fcd2f41df718de9c25
Fabiana <139273452+FaJuEs@users.noreply.github.com>
2023-11-22 16:55:05 -0300

    Add files via upload

--- src/components/ProductoDetalle.jsx
+++ src/components/ProductoDetalle.jsx
@@ -31,7 +31,7 @@ const ProductoDetalle = ({
         Comprar
       </button>
 
-      {compraRealizada && <p>Gracias por su compra</p>}
+      {compraRealizada && <p> GRACIAS POR SU COMPRA !!!</p>}
     </div>
   );
 };
@@ -45,34 +45,3 @@ ProductoDetalle.propTypes = {
 };
 
 export default ProductoDetalle;
-
-/* import PropTypes from 'prop-types';
-import NombreProducto from './NombreProducto';
-import DescripcionProducto from './DescripcionProducto';
-import PrecioProducto from './PrecioProducto';
-import ProductoSKU from './ProductoSKU';
-import CantidadDisponible from './CantidadDisponible';
-
-const ProductoDetalle = ({nombre, descripcion, precio, sku, cantidadDisponible}) => {
-  return (
-    <div className='detalle'>
-      
-      <NombreProducto nombre={nombre} />
-      <DescripcionProducto descripcion={descripcion} />
-      <PrecioProducto precio={precio} />
-      <ProductoSKU sku={sku} />
-      <CantidadDisponible cantidad={cantidadDisponible} />
-    </div>
-  );
-};
-
-ProductoDetalle.propTypes = {
-  nombre: PropTypes.string.isRequired,
-  descripcion: PropTypes.string.isRequired,
-  precio: PropTypes.number.isRequired,
-  sku: PropTypes.string.isRequired,
-  cantidadDisponible: PropTypes.number.isRequired,
-};
-
-export default ProductoDetalle;
- */
